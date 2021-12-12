3c4ae416325bee71147968ad9774218fdd8a0cfc
VladSKN <vladsemikin3@gmail.com>
2021-12-12 19:09:35 +0200

    1.2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,6 @@
 
 public class Main {
     public static void main(String[] args) {
-        Server server = new Server(9999, 64);
-        server.start();
+        new Server(9999, 64).start();
     }
 }
